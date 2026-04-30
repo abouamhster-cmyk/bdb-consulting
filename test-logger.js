fc62bb2bc8cbf3689083e3d60af8f417187553e9
abouamhster-cmyk <abouamhster@gmail.com>
2026-04-30 13:21:35 +0200

    Delete test-logger.js

--- test-logger.js
+++
@@ -1,7 +0,0 @@
-const { logger } = require("./app/lib/logger.js");
-
-logger.info("Test info message");
-logger.error("Test error message");
-logger.warn("Test warning message");
-
-logger.info({ userId: "123", action: "login" }, "User logged in");
