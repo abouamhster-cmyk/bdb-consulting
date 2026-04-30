553f614faf494896b141a54caf9907585e81031c
abouamhster-cmyk <abouamhster@gmail.com>
2026-04-30 09:49:10 +0200

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,14 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Valeurs en dur pour le test (à remplacer après)
 const supabaseUrl = 'https://yivzprlnhjkyldszmcns.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpdnpwcmxuaGpreWxkc3ptY25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MDAyMjgsImV4cCI6MjA5MTQ3NjIyOH0.p7vQMuHSDu7ZH4_mfvD4z9uk73xglwfOHCRJV-9vY50'
 
-// Configuration explicite avec les en-têtes
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true
   },
   global: {
     headers: {
